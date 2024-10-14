b802ce5b875367a626e5d76f8307cc1abc17a0d1
Barasa-Micah <barasamica@gmail.com>
2024-10-15 01:06:55 +0300

    Newsletter Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import HomeSection from './components/HomeSection';
 import InDemandSection from './components/InDemandSection';
 import FindLocateSection from './components/FindLocateSection'; // Import the new section
+import NewsletterSection from './components/NewsletterSection';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <HomeSection />
       <InDemandSection />
       <FindLocateSection /> {/* Include the new section here */}
-      
+      <NewsletterSection />
     </div>
   );
 }
